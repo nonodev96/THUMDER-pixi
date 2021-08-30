504adb426c03831e72a19be43d07ad8ac449a857
amm00220 <amm00220@red.ujaen.es>
2021-08-30 17:38:20 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
 pixi-pipeline
 =============
+
+## Define PIXI Application
+
+```ts
+const app = new PIXI.Application({
+  width: SIZE * 2,
+  height: SIZE,
+  backgroundColor: 0xDDDDDD,
+  sharedTicker: true,
+  sharedLoader: true,
+  antialias: true,
+  autoDensity: true,
+  resolution: 2,
+});
+app.view.id = 'main';
+```
+
+## Pipeline Methods
+
+```ts
+const pipeline = new PixiJSPipeline();
+
+let iter = 0;
+pipeline.addInstruction('ADDI R28, R0, #4', {}, iter);
+iter++;
+pipeline.addInstruction('ADDI R17, R0, 0', {}, iter);
+iter++;
+pipeline.addInstruction('ADDI R18, R0, 0', {}, iter);
+iter++;
+pipeline.addInstruction('ADDI R26, R0, #32', {}, iter);
+iter++;
+pipeline.addInstruction('ADDI R29, R0, #1', { ID_stall: 2 }, iter);
+pipeline.addArrow({
+  start: {
+    instruction: 1,
+    step: 1,
+  },
+  to: {
+    instruction: 2,
+    step: 2,
+  },
+});
+
+pipeline.nextStep();
+pipeline.debug();
+```
+
+### Add Pipeline interface
+
+```ts
+app.stage.addChild(pipeline.draw());
+```
+
+## Pixi init
+
+```ts
+let state;
+
+function play(delta) {
+  const speed = 5 * delta;
+  if (Keyboard.isKeyDown('ArrowLeft', 'KeyA', 'KeyJ')) {
+    pipeline.moveRight();
+  }
+  if (Keyboard.isKeyDown('ArrowRight', 'KeyD', 'KeyL')) {
+    pipeline.moveLeft();
+  }
+  if (Keyboard.isKeyDown('ArrowUp', 'KeyW', 'KeyI')) {
+    pipeline.moveBottom();
+  }
+  if (Keyboard.isKeyDown('ArrowDown', 'KeyS', 'KeyK')) {
+    pipeline.moveTop();
+  }
+}
+
+function gameLoop(delta) {
+  state(delta);
+
+  Keyboard.update();
+  Mouse.update();
+}
+
+function setup() {
+  console.log('Setup');
+
+  // Set the game state
+  state = play;
+
+  // Start the game loop
+  app.ticker.add((delta) => gameLoop(delta));
+
+  Mouse.events.on('pressed', null, (buttonCode, event, mouseX, mouseY) => {
+    console.log(buttonCode, event, 'mouseX', mouseX, 'mouseY', mouseY);
+  });
+  Mouse.events.on('released', null, (buttonCode, event, mouseX, mouseY, mouseOriginX, mouseOriginY, mouseMoveX, mouseMoveY) => {
+    console.log(buttonCode, event, 'mouseX', mouseX, 'mouseY', mouseY, 'mouseOriginX', mouseOriginX, 'mouseOriginY', mouseOriginY, 'mouseMoveX', mouseMoveX, 'mouseMoveY', mouseMoveY);
+  });
+}
+
+loader.onProgress.add(() => {
+  console.log('onProgress');
+});
+loader.onError.add(() => {
+  console.log('onError');
+});
+loader.onLoad.add(() => {
+  console.log('onLoad');
+});
+loader.onComplete.add(setup);
+
+loader.load((loader, resources) => {
+  // Add pipeline
+  app.stage.addChild(pipeline.draw());
+
+  const fps = new PIXI.Text('FPS: 0', { fill: 0xffffff });
+  fps.position.x = document.documentElement.clientWidth - 200;
+  fps.position.y = 25;
+
+  app.stage.addChild(fps);
+
+  app.ticker.add((delta) => {
+    fps.text = `FPS: ${ticker.FPS.toFixed(2)}`;
+  });
+});
+```
--- src/app.ts
+++ src/app.ts
@@ -110,40 +110,7 @@ const styleNoWrap = new PIXI.TextStyle({
   dropShadowDistance: 6,
 });
 
-loader.load((loader, resources) => {
-  // Conainer
-  // const table = new PixiJSTable(true, 10, 10, { x: 0, y: 0 }, { x: 0, y: 0 }, new Text(""))
-  // table.addCell(new PIXI.Text("This is a large bit of text without word wrap to show you what happens when there's a large cell", styleNoWrap));
-  // app.stage.addChild(table.draw())
 
-  // Graphics
-  // const grid = new PixiJSGrid(1024, 64, { width: 1, color: 0xffffff, alpha: 1, alignment: 0.5, native: true }, true, true)
-  // app.stage.addChild(grid.drawGrid())
-
-  // Graphics
-  app.stage.addChild(pipeline.draw());
-
-  // const pipeline2 = new PixiJSPipeline(5, 10)
-  // pipeline2.table.position.x += 0
-  // pipeline2.table.position.y += 200
-  // app.stage.addChild(pipeline2.draw())
-
-  const fps = new PIXI.Text('FPS: 0', { fill: 0xffffff });
-  fps.position.x = document.documentElement.clientWidth - 200;
-  fps.position.y = 25;
-
-  app.stage.addChild(fps);
-
-  // const container = new PIXI.Container();
-  // const text = new PIXI.Text("Texto")
-  // container.addChild(text)
-  // app.stage.addChild(container)
-  app.ticker.add((delta) => {
-    fps.text = `FPS: ${ticker.FPS.toFixed(2)}`;
-    // hero.direction = getNextEntityDirection(app.view.width, hero);
-    // hero.sprite.x = getNextEntityPosition(hero);
-  });
-});
 
 /**/
 
@@ -227,4 +194,37 @@ loader.onLoad.add(() => {
   console.log('onLoad');
 });
 loader.onComplete.add(setup);
-loader.load();
+loader.load((loader, resources) => {
+  // Container
+  // const table = new PixiJSTable(true, 10, 10, { x: 0, y: 0 }, { x: 0, y: 0 }, new Text(""))
+  // table.addCell(new PIXI.Text("This is a large bit of text without word wrap to show you what happens when there's a large cell", styleNoWrap));
+  // app.stage.addChild(table.draw())
+
+  // Graphics
+  // const grid = new PixiJSGrid(1024, 64, { width: 1, color: 0xffffff, alpha: 1, alignment: 0.5, native: true }, true, true)
+  // app.stage.addChild(grid.drawGrid())
+
+  // Graphics
+  app.stage.addChild(pipeline.draw());
+
+  // const pipeline2 = new PixiJSPipeline(5, 10)
+  // pipeline2.table.position.x += 0
+  // pipeline2.table.position.y += 200
+  // app.stage.addChild(pipeline2.draw())
+
+  const fps = new PIXI.Text('FPS: 0', { fill: 0xffffff });
+  fps.position.x = document.documentElement.clientWidth - 200;
+  fps.position.y = 25;
+
+  app.stage.addChild(fps);
+
+  // const container = new PIXI.Container();
+  // const text = new PIXI.Text("Texto")
+  // container.addChild(text)
+  // app.stage.addChild(container)
+  app.ticker.add((delta) => {
+    fps.text = `FPS: ${ticker.FPS.toFixed(2)}`;
+    // hero.direction = getNextEntityDirection(app.view.width, hero);
+    // hero.sprite.x = getNextEntityPosition(hero);
+  });
+});
